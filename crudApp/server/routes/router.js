05ef033cf5eef80fadb12085af67dc53945fff35
CTHall05 <63585450+CTHall05@users.noreply.github.com>
2023-02-19 22:30:28 +0000

    Api working and tested with postman all routes and functions work

--- crudApp/server/routes/router.js
+++ crudApp/server/routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const route = express.Router();
 
 const services = require('../services/render.js');
-const controller = require('../database/connection.js');
+const controller = require('../controller/controller.js');
 
 /**
  * @description Root Route
@@ -26,6 +26,6 @@ route.get('/update-user', services.update_user);
 route.post('/api/users', controller.create);
 route.get('/api/users', controller.find);
 route.put('/api/users/:id', controller.update);
-route.delete('/api/users', controller.delete);
+route.delete('/api/users/:id', controller.delete);
  
 module.exports = route;
